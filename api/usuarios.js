b29e2ce2112a8a5d5621dea6d5eada8e9be9508c
JvPelai <61389657+JvPelai@users.noreply.github.com>
2020-10-15 16:47:03 -0300

    listagem de usuarios e dados de usuario

--- api/usuarios.js
+++ api/usuarios.js
@@ -5,8 +5,10 @@ var pagina = document.querySelectorAll('a');
 function listaUsuarios(page){
     return fetch(`https://api.github.com/search/users?q=location:Piracicaba&page=${page}`)
     .then( response => {
+        console.log(response)
         return response.json();
     }).then( json => {
+        console.log(json)
         return json.items
     })
 }
@@ -21,19 +23,21 @@ function paginaUsuarios(page){
     lista.innerHTML = ""
     listaUsuarios(page).then( list => {
         list.forEach(user => {
-            var userNameItem = document.createElement('li');
-            userNameItem.textContent = user.login;
-            lista.appendChild(userNameItem);
+            var userCard = createUserCard(user);
+            lista.appendChild(userCard);
         });
     });
 }
 
-function userData(name){
-    return fetch(`https://api.github.com/users/${name}`)
-    .then( response => {
-        console.log(response)
-        return response.json;
-    }).then( json => {
-        return json
-    })
+function createUserCard(user){
+    var card = document.createElement("div");
+    card.classList.add('user-card')
+    var avatar = document.createElement("div");
+    var name = document.createElement("div");
+    avatar.innerHTML = `<img src="${user.avatar_url}">`
+    name.innerHTML = `[<a href="/user.html?username=${user.login}">${user.login}</a>]`;
+    card.appendChild(avatar);
+    card.appendChild(name);
+    return card
 }
+
