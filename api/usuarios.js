02fd2fe6a2d8fcc2aef5ac27d8947efe54ff82eb
Paulo Oliveira <paulooliveira@pecege.com>
2020-11-01 12:29:22 -0300

    ajustes e comentarios para melhorias no código

--- api/usuarios.js
+++ api/usuarios.js
@@ -1,159 +1,169 @@
-
-
-function listaUsuarios(page="1",type="User",filtros=""){
-    currentPage = page;
-    return fetch(`https://api.github.com/search/users?q=${searchBar.value}+type:${type}+location:Piracicaba${filtros}${sortParams}&per_page=20&page=${page}`,fetchParams)
-    .then( response => {
+function listaUsuarios(page = "1", type = "User", filtros = "") {
+  currentPage = page;
+  return (
+    fetch(
+      `https://api.github.com/search/users?q=${searchBar.value}+type:${type}+location:Piracicaba${filtros}${sortParams}&per_page=20&page=${page}`,
+      fetchParams
+    )
+      .then((response) => {
         return response.json();
-    }).then( json => {
-        if(json.total_count == 0){
-            noResults();
-            return
-        }else{
-            errorMsg.classList.add("no-results");
-            paginate(json.total_count);
+      })
+      // TODO: a responsabilidade dessa função é retornar a lista, quem chama ela, deve saber o que fazer caso não receba dados
+      .then((json) => {
+        if (json.total_count == 0) {
+          noResults();
+          return;
+        } else {
+          errorMsg.classList.add("no-results");
+          paginate(json.total_count);
         }
         return json.items;
-    })
+      })
+  );
 }
 
-function noResults(){
-    errorMsg.classList.remove("no-results");
-    telaCarregamento.classList.add("hidden-order");
+function noResults() {
+  errorMsg.classList.remove("no-results");
+  telaCarregamento.classList.add("hidden-order");
 }
 
-
-async function paginaUsuarios(page,type="User",filtros=""){
-    lista.innerHTML = "";
-    currentPage = page;
-    telaCarregamento.classList.remove("hidden-order");
-    if(!sortCheckBox.checked){
-        await createUserArray().then(array => {
-            for(let i = 0; i < 20; i++){
-                if(lista.childElementCount == array.length){
-                    return
-                };
-                if(array.length == 1){
-                    var userCard = createUserCard(array[0]);
-                    lista.appendChild(userCard);
-                    return
-                };
-                var index = i + ((parseInt(page) -1) * 20);
-                var userCard = createUserCard(array[index]);
-                lista.appendChild(userCard); 
-            };
-            
-        }); 
-    }else{
-        await listaUsuarios(page,type,filtros).then( list => {
-            list.forEach(user => {
-                var userCard = createUserCard(user);
-                lista.appendChild(userCard);      
-            });
-        });
-    }
-    telaCarregamento.classList.add("hidden-order");
-};       
-
-
-
-
-
-lista.addEventListener("scroll",async function(){
-    if(parseInt(currentPage) > pageList.childElementCount){
-        return
-    };
-    if(lista.scrollHeight == (lista.offsetHeight + lista.scrollTop)){
-        if(!sortCheckBox.checked){
-            await createUserArray().then(array => {
-                currentPage = String(parseInt(currentPage) + 1);
-                for(let i = 0; i < 20; i++){
-                    if(lista.childElementCount == array.length){
-                        return
-                    }
-                    var userCard = createUserCard(array[lista.childElementCount]);
-                    lista.appendChild(userCard); 
-                };
-            }) 
-        }else{
-            listaUsuarios(parseInt(currentPage)+1,searchTypeParam,filtros).then( list => {
-                list.forEach(user => {
-                    var userCard = createUserCard(user);
-                    lista.appendChild(userCard);
-                });
-            });
+async function paginaUsuarios(page, type = "User", filtros = "") {
+  lista.innerHTML = "";
+  currentPage = page;
+  telaCarregamento.classList.remove("hidden-order");
+  if (!sortCheckBox.checked) {
+    await createUserArray().then((array) => {
+      for (let i = 0; i < 20; i++) {
+        if (lista.childElementCount == array.length) {
+          return;
         }
-    };
+        if (array.length == 1) {
+          var userCard = createUserCard(array[0]);
+          lista.appendChild(userCard);
+          return;
+        }
+        var index = i + (parseInt(page) - 1) * 20;
+        var userCard = createUserCard(array[index]);
+        lista.appendChild(userCard);
+      }
+    });
+  } else {
+    // await listaUsuarios(page,type,filtros).then( list => {
+    //     list.forEach(user => {
+    //         var userCard = createUserCard(user);
+    //         lista.appendChild(userCard);
+    //     });
+    // });
 
+    // HACK: nunca usar await e then juntos isso não é uma boa pratica. Se possivel use sempre async/await.
+    // Veja no exemplo abaixo, como o codigo fica até mais organizado.
+    const list = await listaUsuarios(page, type, filtros);
+    list.forEach((user) => {
+      var userCard = createUserCard(user);
+      lista.appendChild(userCard);
+    });
+  }
+  telaCarregamento.classList.add("hidden-order");
+}
+
+lista.addEventListener("scroll", async function () {
+  if (parseInt(currentPage) > pageList.childElementCount) {
+    return;
+  }
+  if (lista.scrollHeight == lista.offsetHeight + lista.scrollTop) {
+    if (!sortCheckBox.checked) {
+      await createUserArray().then((array) => {
+        currentPage = String(parseInt(currentPage) + 1);
+        for (let i = 0; i < 20; i++) {
+          if (lista.childElementCount == array.length) {
+            return;
+          }
+          var userCard = createUserCard(array[lista.childElementCount]);
+          lista.appendChild(userCard);
+        }
+      });
+    } else {
+      const list = await listaUsuarios(
+        parseInt(currentPage) + 1,
+        searchTypeParam,
+        filtros
+      );
+      list.forEach((user) => {
+        var userCard = createUserCard(user);
+        lista.appendChild(userCard);
+      });
+    }
+  }
 });
 
-
-function createUserCard(user){
-    var card = document.createElement("div");
-    card.classList.add('user-card')
-    var avatar = document.createElement("div");
-    var name = document.createElement("div");
-    avatar.innerHTML = `<img src="${user.avatar_url}">`
-    name.innerHTML = `<a href="/user.html?username=${user.login}">${user.login}</a>`;
-    card.appendChild(avatar);
-    card.appendChild(name);
-    return card
+function createUserCard(user) {
+  var card = document.createElement("div");
+  card.classList.add("user-card");
+  var avatar = document.createElement("div");
+  var name = document.createElement("div");
+  avatar.innerHTML = `<img src="${user.avatar_url}">`;
+  name.innerHTML = `<a href="./user.html?username=${user.login}">${user.login}</a>`;
+  card.appendChild(avatar);
+  card.appendChild(name);
+  return card;
 }
 
+function orderByCreation() {}
 
-
-function orderByCreation(){
-   
+function ordenaData(a, b) {
+  if (ordemCadastro.value == "mais-antigos") {
+    return a.created_at < b.created_at
+      ? -1
+      : a.created_at > b.created_at
+      ? 1
+      : 0;
+  } else {
+    return b.created_at < a.created_at
+      ? -1
+      : b.created_at > a.created_at
+      ? 1
+      : 0;
+  }
 }
 
-function ordenaData(a,b){
-    if(ordemCadastro.value == "mais-antigos"){
-        return a.created_at < b.created_at ? -1 : a.created_at > b.created_at ? 1 : 0;
-    }else{
-        return b.created_at < a.created_at ? -1 : b.created_at > a.created_at ? 1 : 0;
-    }    
-};
-
-function userData(name){
-    return fetch(`https://api.github.com/users/${name}`,fetchParams)
-    .then( response => {
-        return response.json();
-    }).then( json => {
-        return json
+function userData(name) {
+  return fetch(`https://api.github.com/users/${name}`, fetchParams)
+    .then((response) => {
+      return response.json();
     })
-};
+    .then((json) => {
+      return json;
+    });
+}
 
-
-
-async function createUserArray(){
-    var arr = [];
-    if(!localStorage.getItem("userObjectArray")){
-        for(let i = 1; i <= pageList.children.length + 1 ; i++){
-            await listaUsuarios(String(i),searchTypeParam,filtros).then(list => {
-                if(list.length == 1){
-                    arr.push(list[0]);
-                    return
-                };
-                list.forEach( user => {
-                    userData(user.login).then( userProf => {
-                        arr.push(userProf);
-                    });
-                });
-            });
-        };
-        localStorage.setItem("userObjectArray",JSON.stringify(arr));
-        currentPage = "1";
-        if(arr.length <= 1){
-            return arr
-        }else{
-            return arr.sort(ordenaData)
-        };
-    }else{
-        return JSON.parse(localStorage.getItem("userObjectArray")).sort(ordenaData)
-    }; 
-    
-};
-
+// TODO: remover essa função. Ela é desnecessária, só existe por causa do checkbox que deve ser removido tb
+async function createUserArray() {
+  var arr = [];
+  if (!localStorage.getItem("userObjectArray")) {
+    for (let i = 1; i <= pageList.children.length + 1; i++) {
+      await listaUsuarios(String(i), searchTypeParam, filtros).then((list) => {
+        if (list.length == 1) {
+          arr.push(list[0]);
+          return;
+        }
+        list.forEach((user) => {
+          userData(user.login).then((userProf) => {
+            arr.push(userProf);
+          });
+        });
+      });
+    }
+    localStorage.setItem("userObjectArray", JSON.stringify(arr));
+    currentPage = "1";
+    if (arr.length <= 1) {
+      return arr;
+    } else {
+      return arr.sort(ordenaData);
+    }
+  } else {
+    return JSON.parse(localStorage.getItem("userObjectArray")).sort(ordenaData);
+  }
+}
 
 paginaUsuarios(currentPage);
 
--- main.js
+++ main.js
@@ -1,204 +1,215 @@
+// TODO: organizar sempre as variaveis globais (utilizadas em mais de um arquivo), no topo do script
 var errorMsg = document.querySelector(".no-results");
 var currentPage = "1";
-var telaCarregamento = document.querySelector(".tela-carregamento")
-
-var accessToken = {token: "50aca22dda5474d5a46ca31ab0831ad883488aab"};
-var auth = accessToken.token;
-
-var fetchParams = {
-    method: 'GET',
-    headers: {
-        Authorization: 'Basic ' + btoa(auth),
-        
-    },
-  };
-
-
-
-fetch("https://api.github.com/rate_limit",fetchParams).then( response => {return response.json()}).then(json => {console.log(json)});
-
+var telaCarregamento = document.querySelector(".tela-carregamento");
 var lista = document.querySelector(".lista-usuarios");
 var pageList = document.querySelector(".paginas");
-
-
-
-function paginate(totalCount){
-    var pags = Math.round(totalCount / 20);
-    pageList.innerHTML = "";
-    for(let i = 1; i <= pags; i++){
-        let pag = document.createElement("li");
-        pag.innerHTML = `<a class="pagina">${i}</a>`;
-        pageList.appendChild(pag);
-    }
-    var pagina = document.querySelectorAll('.pagina');
-    pagina.forEach(p => {
-        p.addEventListener("click", function(event){
-            event.preventDefault();
-            paginaUsuarios(p.textContent,searchTypeParam,filtros)
-            
-        });
-    });
-}
-var filtros = "";
-
-var tiposDeBusca = document.getElementsByName("tipo-busca");
-var searchTypeParam;
-
-
-for(tipo of tiposDeBusca){
-    if(tipo.checked){
-        searchTypeParam = tipo.value
-    }
-    tipo.addEventListener('change', function(){
-        searchTypeParam = this.value;
-    })
-};
-
 var orderCadBox = document.querySelector(".ordem-cadastro");
-var sortBox = document.querySelector("#sort-box")
+var sortBox = document.querySelector("#sort-box");
 var sortCheckBox = document.querySelector("#sort-results");
 var sortOptions = document.getElementsByName("sort-option");
 var sortOrderOptions = document.getElementsByName("sort-order");
 var sortParams = "";
 var sortOption;
 var sortOrder;
+var searchTypeParam;
+var filtros = "";
 
-function changeSortParams(){
-    if(sortCheckBox.checked){
-        sortParams = sortOption + sortOrder
-    }else{
-        sortParams = "";
-    }
+const accessToken = { token: "50aca22dda5474d5a46ca31ab0831ad883488aab" };
+const auth = accessToken.token;
+const fetchParams = {
+  method: "GET",
+  headers: {
+    Authorization: "Basic " + btoa(auth),
+  },
 };
 
-for(option of sortOptions){
-    if(option.checked){
-        sortOption = option.value;
-    }
-    option.addEventListener('change', function(){
-        sortOption = this.value;
-        changeSortParams()
-    })
-};
-for(option of sortOrderOptions){
-    if(option.checked){
-        sortOrder = option.value;
-    }
-    option.addEventListener('change', function(){
-        sortOrder = this.value;
-        changeSortParams()
-    })
+function fetchRateLimit() {
+// HACK: não há necessidade de esperar a resposta dessa requisição se ela não é retornada pra ninguem
+//   fetch("https://api.github.com/rate_limit", fetchParams)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//     });
+
+    fetch("https://api.github.com/rate_limit", fetchParams);    
+}
+// HACK: é sempre bom deixar claro o momento em que são chamadas as funções. Não é boa pratica deixar fetches soltos pela aplicação
+fetchRateLimit();
+
+function paginate(totalCount) {
+  var pags = Math.round(totalCount / 20);
+  pageList.innerHTML = "";
+  for (let i = 1; i <= pags; i++) {
+    let pag = document.createElement("li");
+    pag.innerHTML = `<a class="pagina">${i}</a>`;
+    pageList.appendChild(pag);
+  }
+  var pagina = document.querySelectorAll(".pagina");
+  pagina.forEach((p) => {
+    p.addEventListener("click", function (event) {
+      event.preventDefault();
+      paginaUsuarios(p.textContent, searchTypeParam, filtros);
+    });
+  });
 }
 
-sortCheckBox.addEventListener("change", function(){ 
-    if(this.checked){
-        sortParams = sortOption + sortOrder
-        sortBox.classList.remove("hidden-order");
-        orderCadBox.classList.add("hidden-order");
-    }else{
-        sortParams = "";
-        sortBox.classList.add("hidden-order");
-        orderCadBox.classList.remove("hidden-order");
-    }
+var tiposDeBusca = document.getElementsByName("tipo-busca");
+for (tipo of tiposDeBusca) {
+  if (tipo.checked) {
+    searchTypeParam = tipo.value;
+  }
+  tipo.addEventListener("change", function () {
+    searchTypeParam = this.value;
+  });
+}
+
+function changeSortParams() {
+  if (sortCheckBox.checked) {
+    sortParams = sortOption + sortOrder;
+  } else {
+    sortParams = "";
+  }
+}
+
+// TODO: organizar melhor a ordem de execução, o código está muito largado.
+// criar funções com nome claro ex: addEventListenerToSortOptions 
+for (option of sortOptions) {
+  if (option.checked) {
+    sortOption = option.value;
+  }
+  option.addEventListener("change", function () {
+    sortOption = this.value;
+    changeSortParams();
+  });
+}
+for (option of sortOrderOptions) {
+  if (option.checked) {
+    sortOrder = option.value;
+  }
+  option.addEventListener("change", function () {
+    sortOrder = this.value;
+    changeSortParams();
+  });
+}
+
+sortCheckBox.addEventListener("change", function () {
+  if (this.checked) {
+    sortParams = sortOption + sortOrder;
+    sortBox.classList.remove("hidden-order");
+    orderCadBox.classList.add("hidden-order");
+  } else {
+    sortParams = "";
+    sortBox.classList.add("hidden-order");
+    orderCadBox.classList.remove("hidden-order");
+  }
 });
-
-
 
 //Ordem de cadastro
 var ordemCadastro = document.querySelector("#ordem-cadastro");
-
 
 var searchBar = document.querySelector("#main-search");
 var searchBtn = document.querySelector(".search-btn");
 
 let timeout = null;
 
-searchBar.addEventListener("input", function(event){
-    event.preventDefault();
-    clearTimeout(timeout);
-    timeout = setTimeout(function(){
-        localStorage.clear();
-        paginaUsuarios("1",searchTypeParam,filtros);
-    },1000);    
+searchBar.addEventListener("input", function (event) {
+  event.preventDefault();
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    localStorage.clear();
+    paginaUsuarios("1", searchTypeParam, filtros);
+  }, 1000);
 });
 
-searchBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    localStorage.clear();
-    paginaUsuarios("1",searchTypeParam,filtros);
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  localStorage.clear();
+  paginaUsuarios("1", searchTypeParam, filtros);
 });
 
 var submitBtn = document.querySelector("#filter-submit");
 
-function getFormData(){
-    var filtroDataCheck = document.getElementById("filtro-data");
-    var filtroSegCheck = document.getElementById("filtro-seg");
-    var filtroReposCheck = document.getElementById("filtro-repos");
-    var maxDataCad = document.getElementById("max-data-cad").value;
-    var minDataCad = document.getElementById("min-data-cad").value;
+function getFormData() {
+  var filtroDataCheck = document.getElementById("filtro-data");
+  var filtroSegCheck = document.getElementById("filtro-seg");
+  var filtroReposCheck = document.getElementById("filtro-repos");
+  var maxDataCad = document.getElementById("max-data-cad").value;
+  var minDataCad = document.getElementById("min-data-cad").value;
 
-    if(maxDataCad == "undefined" || maxDataCad == ""){
-        var today = new Date();
+  if (maxDataCad == "undefined" || maxDataCad == "") {
+    var today = new Date();
 
-        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
 
-        maxDataCad = date;
-    }
+    maxDataCad = date;
+  }
 
+  if (filtroSegCheck.checked) {
+    var minSeguidores = document.getElementById("min-seguidores").value;
+    var maxSeguidores = document.getElementById("max-seguidores").value;
+  }
 
+  if (filtroReposCheck.checked) {
+    var minRepos = document.getElementById("min-repos").value;
+    var maxRepos = document.getElementById("max-repos").value;
+  }
 
-    if(filtroSegCheck.checked){
-        var minSeguidores = document.getElementById("min-seguidores").value;
-        var maxSeguidores = document.getElementById("max-seguidores").value;
-    };
-
-    if(filtroReposCheck.checked){
-        var minRepos = document.getElementById("min-repos").value;
-        var maxRepos = document.getElementById("max-repos").value;
-        
-    }
-
-    var filters ={filtroDataCheck,filtroSegCheck,filtroReposCheck,minDataCad,maxDataCad,minSeguidores,maxSeguidores,minRepos,maxRepos};
-    return filters
-    
+  var filters = {
+    filtroDataCheck,
+    filtroSegCheck,
+    filtroReposCheck,
+    minDataCad,
+    maxDataCad,
+    minSeguidores,
+    maxSeguidores,
+    minRepos,
+    maxRepos,
+  };
+  return filters;
 }
 
-function buildQueryString(filters){
-    var queryString = ""; 
-    if(filters.filtroDataCheck.checked){
-        var dataCad = `+created:${filters.minDataCad}..${filters.maxDataCad}`;
-        queryString += dataCad;
+function buildQueryString(filters) {
+  var queryString = "";
+  if (filters.filtroDataCheck.checked) {
+    var dataCad = `+created:${filters.minDataCad}..${filters.maxDataCad}`;
+    queryString += dataCad;
+  }
+  if (filters.filtroSegCheck.checked) {
+    if (filters.minSeguidores == "") {
+      filters.minSeguidores = "0";
     }
-    if(filters.filtroSegCheck.checked){
-        if(filters.minSeguidores == ""){
-            filters.minSeguidores = "0"
-        };
-        if(filters.maxSeguidores == ""){
-            filters.maxSeguidores = "999999999"
-        };
-        var seguidores = `+followers:${filters.minSeguidores}..${filters.maxSeguidores}`;
-        queryString += seguidores;
+    if (filters.maxSeguidores == "") {
+      filters.maxSeguidores = "999999999";
     }
-    if(filters.filtroReposCheck.checked){
-        if(filters.minRepos == ""){
-            filters.minRepos = "0"
-        };
-        if(filters.maxRepos == ""){
-            filters.maxRepos = "999999999"
-        };
-        var repos = `+repos:${filters.minRepos}..${filters.maxRepos}`;
-        queryString += repos;
+    var seguidores = `+followers:${filters.minSeguidores}..${filters.maxSeguidores}`;
+    queryString += seguidores;
+  }
+  if (filters.filtroReposCheck.checked) {
+    if (filters.minRepos == "") {
+      filters.minRepos = "0";
     }
-    return queryString;
+    if (filters.maxRepos == "") {
+      filters.maxRepos = "999999999";
+    }
+    var repos = `+repos:${filters.minRepos}..${filters.maxRepos}`;
+    queryString += repos;
+  }
+  return queryString;
 }
 
-submitBtn.addEventListener("click",function(event){
-    event.preventDefault();
-    if(!sortCheckBox.checked){
-        localStorage.clear();
-    }
-    filtros = buildQueryString(getFormData());
-    paginaUsuarios("1",searchTypeParam,filtros);  
-})
-
-
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (!sortCheckBox.checked) {
+    localStorage.clear();
+  }
+  filtros = buildQueryString(getFormData());
+  paginaUsuarios("1", searchTypeParam, filtros);
+});
